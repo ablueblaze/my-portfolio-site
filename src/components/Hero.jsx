9c3ae7f54a372b013494232bd80ab6198995d59d
Blaze Scott <blazeascott@gmail.com>
2022-08-26 03:12:41 -0500

    Hero section started

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
 export default function Hero() {
-  return <div></div>;
+  return (
+    <div>
+      <div className='border-2 border-solid border-black container mx-auto flex items-center justify-center relative'>
+        <h1 className=''>Example Title</h1>
+        <img
+          className='w-3/12'
+          src='../src/assets/feather-pointed-solid.svg'
+          alt='feather icon'
+        />
+      </div>
+    </div>
+  );
 }
