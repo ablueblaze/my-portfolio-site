ac340714465ae3d499b215437eb2f8478a00b22f
Blaze Scott <blazeascott@gmail.com>
2022-09-15 02:18:33 -0500

    Removed modals

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { nanoid } from 'nanoid';
 
-export default function Projects({ projects, ProjectModal, toggleProject }) {
+export default function Projects({ projects }) {
   return (
     <div>
       <h2 className='pt-32 pb-16 text-7xl underline' id='projects'>
@@ -11,25 +11,14 @@ export default function Projects({ projects, ProjectModal, toggleProject }) {
         {projects.map((project) => {
           const currentId = nanoid();
           return (
-            <div
-              key={currentId}
-              onMouseEnter={(e) => toggleProject(e, currentId)}
-              onMouseLeave={(e) => toggleProject(e, currentId)}>
+            <a href={project.link} target='_blank' key={currentId}>
               <img
                 className='rounded-3xl shadow-2xl w-44 h-44 object-cover m-4'
                 // className='max-w-full h-auto rounded-full'
                 src={project.image}
                 alt={project.alt}
               />
-              <ProjectModal
-                id={currentId}
-                title={project.title}
-                webImage={project.image}
-                webAlt={project.alt}
-                link={project.link}
-                description={project.description}
-              />
-            </div>
+            </a>
           );
         })}
       </div>
